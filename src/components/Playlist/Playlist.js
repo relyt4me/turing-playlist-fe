19fc6c08f0083f8339dd70ed0cb2263419def679
Tyler Haglund <relyt4me@gmail.com>
2020-09-02 09:40:20 -0600

    Export Playlist

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import SongCard from '../Song-card/Song-card'
+import SongCard from '../Song-card/Song-card';
 
-const Playlist({songs}) => {
-  const songCards = songs.map(song => {
-    return (
-      <SongCard song={song} key={song.id}/>
-    )
-  })
+const Playlist = ({ songs }) => {
+  const songCards = songs.map((song) => {
+    return <SongCard song={song} key={song.id} />;
+  });
 
   return (
     <section className='playlist'>
       <h2 className='playlist-title'>My Playlist</h2>
-      <div className='playlist-container'>
-        {songCards}
-      </div>
+      <div className='playlist-container'>{songCards}</div>
     </section>
-  )
-}
+  );
+};
 
 export default Playlist;
