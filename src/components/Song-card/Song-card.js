4eb0cf1d1d90587aacf59b80429cd310dae62f99
Tyler Haglund <relyt4me@gmail.com>
2020-09-02 09:31:51 -0600

    Create song card that takes in a song

---
+++ src/components/Song-card/Song-card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SongCard = ({ song }) => {
+  return (
+    <div className={'song-card'}>
+      <h2>{song.songName}</h2>
+      <h3>by {song.artistName}</h3>
+      <a href={song.link}>See Video</a>
+    </div>
+  );
+};
+
+export default SongCard;
