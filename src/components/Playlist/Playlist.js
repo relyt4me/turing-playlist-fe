40c9471319957dd292be5e2ce9544250fe5e9517
Tyler Haglund <relyt4me@gmail.com>
2020-09-02 09:37:01 -0600

    Map over songs to make songcards

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SongCard from '../Song-card/Song-card'
+
+const Playlist({songs}) => {
+  const songCards = songs.map(song => {
+    return (
+      <SongCard song={song}/>
+    )
+  })
+
+  return (
+    <section className='playlist'>
+      <h2 className='playlist-title'></h2>
+    </section>
+  )
+}
