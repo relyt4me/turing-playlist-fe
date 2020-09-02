59b273f1f9dca9fa8aae97ddc167268bca3c4237
Tyler Haglund <relyt4me@gmail.com>
2020-09-02 09:47:17 -0600

    Get playlist on mount

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 import SongController from '../SongController/SongController';
+import { getPlaylist } from '../apiCalls';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      songQueue: []
-    }
+      songQueue: [],
+      error: '',
+    };
   }
 
+  componentDidMount() {
+    getPlaylist()
+      .then((playlist) => {
+        this.setState({ songQueue: playlist });
+      })
+      .catch((error) => this.setState({ error }));
+  }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className='App'>
+        <header className='App-header'>
           <h1>Turing Playlist</h1>
         </header>
-        <div className="App-background">
-          <main>
-          </main>
-        </div> 
+        <div className='App-background'>
+          <main></main>
+        </div>
       </div>
-    )
+    );
   }
 }
 
