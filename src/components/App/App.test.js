d9ca3d2d0794916139cfebb27500dc9ad42e3eca
Tyler Haglund <relyt4me@gmail.com>
2020-09-02 12:03:46 -0600

    Create test for user adding a song

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,7 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { getPlaylist } from '../apiCalls';
 import App from './App';
+jest.mock('../apiCalls.js');
 
-test('true should be true', () => {
-  expect(true).toEqual(true);
+describe('App Component', () => {
+  it('Should allow a user to add a song to the playlist', () => {
+    getPlaylist.mockResolvedValue([]);
+    render(<App />);
+
+    const inputForName = screen.getByPlaceholderText('Name');
+    const inputForArtist = screen.getByPlaceholderText('Artist');
+    const inputForLink = screen.getByPlaceholderText('Link');
+    const submitButton = screen.getByRole('button', { name: 'Add Song' });
+
+    fireEvent.change(inputForName, { target: { value: 'King' } });
+    fireEvent.change(inputForArtist, { target: { value: 'Years and Years' } });
+    fireEvent.change(inputForLink, { target: { value: 'www.google.com' } });
+    fireEvent.click(submitButton);
+
+    const songName = screen.getByRole('heading', { name: 'King' });
+    const songArtist = screen.getByRole('heading', { name: 'by Years and Years' });
+
+    expect(songName).toBeInTheDocument();
+    expect(songArtist).toBeInTheDocument();
+  });
 });
