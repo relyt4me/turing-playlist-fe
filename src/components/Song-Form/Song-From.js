d6bcc46003b3b0ce0c1342e8fde87f05f5052c5b
Tyler Haglund <relyt4me@gmail.com>
2020-09-02 10:15:23 -0600

    Create layout for form

--- src/components/Song-Form/Song-From.js
+++ src/components/Song-Form/Song-From.js
@@ -9,6 +9,21 @@ class SongForm extends Component {
       songLink: '',
     };
   }
+
+  render() {
+    const { songName, songArtist, songLink } = this.state;
+    return (
+      <form className='song-form'>
+        <label htmlFor='song-name'>Song Name</label>
+        <input type='text' placeholder='Name' name='song-name' value={songName} />
+        <label htmlFor='song-artist'>Song Artist</label>
+        <input type='text' placeholder='Artist' name='song-artist' value={songArtist} />
+        <label htmlFor='song-link'>Song Link</label>
+        <input type='text' placeholder='Link' name='song-link' value={songLink} />
+        <button>Add Song</button>
+      </form>
+    );
+  }
 }
 
 export default SongForm;
