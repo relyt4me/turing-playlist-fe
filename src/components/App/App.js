4009c44451dfa8c708cf095daf387281ccf51404
Tyler Haglund <relyt4me@gmail.com>
2020-09-02 09:51:23 -0600

    Render the playlist after get passing the songs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import SongController from '../SongController/SongController';
+import Playlist from '../Playlist/Playlist';
 import { getPlaylist } from '../apiCalls';
 
 class App extends Component {
@@ -17,17 +18,21 @@ class App extends Component {
       .then((playlist) => {
         this.setState({ songQueue: playlist });
       })
-      .catch((error) => this.setState({ error }));
+      .catch((error) => this.setState({ error: 'Something went wrong' }));
   }
 
   render() {
+    const { songQueue, error } = this.state;
     return (
       <div className='App'>
         <header className='App-header'>
           <h1>Turing Playlist</h1>
         </header>
         <div className='App-background'>
-          <main></main>
+          <main>
+            {error && <h1>{error}</h1>}
+            <Playlist songs={songQueue} />
+          </main>
         </div>
       </div>
     );
