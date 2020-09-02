3b4f499a7574edad40215407c0369319b37b64bb
Tyler Haglund <relyt4me@gmail.com>
2020-09-02 09:39:44 -0600

    Put songCards in a container

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -4,14 +4,18 @@ import SongCard from '../Song-card/Song-card'
 const Playlist({songs}) => {
   const songCards = songs.map(song => {
     return (
-      <SongCard song={song}/>
+      <SongCard song={song} key={song.id}/>
     )
   })
 
   return (
     <section className='playlist'>
       <h2 className='playlist-title'>My Playlist</h2>
-      {songCards}
+      <div className='playlist-container'>
+        {songCards}
+      </div>
     </section>
   )
 }
+
+export default Playlist;
